97a55c400d31953f1b925c154925029122290fb0
theoxo <theoxolausson@gmail.com>
2017-05-07 10:38:49 +0100

    v1.1 source update
    See readme for updates

--- MainClass.java
+++ MainClass.java
@@ -2,86 +2,176 @@
  * Created by theoxola on 2017-04-01.
  */
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
 public class MainClass {
 
-    public static MyTreeNode root;
+    private static MyTreeNode root;
+    private static String toFind;
+    private static boolean timeOut;
+    private static long timeLimSecs = 0;
+    private static long tStart;
 
     public static void main(String[] args){
+
+        /**
+         * DEVELOPMENT VERSION
+         */
         /*
-        Board b = new Board();
-        MyTreeNode root = new MyTreeNode(b);
-
-        root.addChild("7813*5642");
-        root.addChild("7823*5641");
-
-        for (MyTreeNode child : root.getChildren()){
-            child.addChild("1");
-        }
-
-        // expected: 1 1 0 2 0
-        System.out.println(MyTreeNode.contains(root, "7823*5641"));
-        System.out.println(MyTreeNode.contains(root, "7813*5642"));
-        System.out.println(MyTreeNode.contains(root, "7813*5641"));
-        System.out.println(MyTreeNode.contains(root, "1"));
-        System.out.println(MyTreeNode.contains(root, "2"));
-
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-        List<MyTreeNode> testList = new ArrayList();
-
-        List<MyTreeNode> test_getNode = new ArrayList();
-
-        MyTreeNode.getNode("1", test_getNode, root);
-
-
-
-        System.out.println("tn " + test_getNode.get(0).getForm());
-
-        List<MyTreeNode> path1 = MyTreeNode.treePath(test_getNode.get(0), testList);
-
-
-        int lvl = 0;
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("PARENT TABLE");
-
-        for (MyTreeNode node : path1) {
-            System.out.println();
-            System.out.println(lvl + "\t" + node.getForm());
-            lvl++;
-        }*/
-
-
-
-
-
         // keep pointer to root
         Board bd = new Board(args[0]);
         root = new MyTreeNode(bd);
+
+        // identify requested end state
         Board find = new Board(args[1]);
+        toFind = find.getLayout();
+        System.out.println("State to find: " + toFind);
 
-        System.out.println("Root loaded with form " +root.getForm());
-        load(root);
+        // begin search
+        System.out.println("Root loaded with form " + root.getForm());
+        load();
+        */
 
-        System.out.println("State to find: " + find.getLayout());
+        /**
+         * END USER VERSION
+         */
 
-        MyTreeNode found = MyTreeNode.getNode(find.getLayout(), root);
+        Scanner in = new Scanner(System.in);
 
-        if(found == null){
-           System.out.println("Could not find node --- no solution could be found!");
+        // initialise and keep pointer to root
+        System.out.print("Please enter your start-state: ");
+        Board bd = new Board(in.next());
+        root = new MyTreeNode(bd);
+        System.out.println("(start date initialised: " + root.getForm() + ")\n");
+
+        // initialise and store requested end state -- run through board to validate
+        System.out.print("Please enter the state you wish to reach: ");
+        Board tf = new Board(in.next());
+        toFind = tf.getLayout();
+        System.out.println("(end state initialised: " + toFind + ")\n");
+
+        System.out.println("Do you want to set a maximum time to allow the code to run for? (y/n)");
+        String ans = in.next();
+
+        while (!ans.equals("y") && !ans.equals("n")){
+            System.out.println("Please answer 'y' or 'n':");
+            ans = in.next();
+        }
+
+        if (ans.equals("y")){
+            timeOut = true;
         }
 
         else {
+            timeOut = false;
+        }
 
-            List<MyTreeNode> parents = new ArrayList();
+        if (timeOut){
+            System.out.print("\nPlease enter the amount of minutes you wish the code to run for; ");
+            do {
+                System.out.print("Whole, positive minutes only, please: ");
+                try {
+                    timeLimSecs = in.nextInt() * 60;
+                } catch (InputMismatchException e) {
+                    // skip over users last input
+                    in.next();
+                }
+            }
+            while (timeLimSecs <= 0);
+        }
+
+        in.close();
+
+        tStart = System.nanoTime();
+
+        // begin search
+        load();
+
+    }
+
+
+
+    private static void load(){
+
+        if (root != null){
+
+            System.out.println();
+            System.out.println("Beginning search for the requested state...");
+            System.out.println();
+
+            Queue<MyTreeNode> queue = new LinkedList<MyTreeNode>();
+
+            queue.add(root);
+
+            boolean stateNotYetFound = true;
+            boolean timeOver = false;
+
+            while(!queue.isEmpty() && stateNotYetFound && !timeOver){
+
+                MyTreeNode node = queue.remove();
+
+                Board base = new Board(node.getForm());
+
+                Board[] nextBoards = Board.nextBoards(base);
+
+                for (Board b : nextBoards){
+
+                    String form = b.getLayout();
+                    MyTreeNode nd = MyTreeNode.getNode(form, root);
+
+                    if (nd == null){
+                        node.addChild(form);
+                        //System.out.println("Board number " + count + " loaded: " + form);
+                       // System.out.println("Board with form " + form + " loaded");
+
+                        // attempt to find the state requested
+                        if(find()){
+                            stateNotYetFound = false;
+                        }
+
+                        else {
+                            //enqueue next
+                            nd = MyTreeNode.getNode(form, root);
+                            queue.add(nd);
+                        }
+
+                    }
+
+                }
+
+                if(timeOut){
+                    long elapsedTime = System.nanoTime() - tStart;
+
+                    if (TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) > timeLimSecs) {
+                        timeOver = true;
+                    }
+                }
+            }
+
+            if (timeOver){
+                System.out.println("!!! No solution could be found in the given amount of time.");
+            }
+
+            System.out.print("\nProgram finished running. Goodbye.");
+        }
+    }
+
+    private static boolean find() {
+
+        MyTreeNode found = MyTreeNode.getNode(toFind, root);
+        boolean toReturn = false;
+
+        if (found != null) {
+            List<MyTreeNode> parents = new ArrayList<>();
 
             List<MyTreeNode> foundPath = MyTreeNode.treePath(found, parents);
+
+            for (int i = 0; i < 3; i++) {
+                System.out.println();
+            }
+            System.out.println("---> REQUESTED STATE FOUND <---");
+            System.out.println();
 
             int lvl = 0;
             for (MyTreeNode node : foundPath) {
@@ -89,55 +179,12 @@ public class MainClass {
                 System.out.println(lvl + "\t" + node.getForm());
                 lvl++;
             }
+
+
+            toReturn = true;
         }
-    }
 
+        return toReturn;
 
-    static int count = 0;
-
-    // Load all possible boards into memory
-    private static void load(MyTreeNode trav){
-
-        Board base = new Board(trav.getForm());
-
-        //Board[] nextBoards = new Board[4];
-
-        // generate next boards
-        Board[] nextBoards = Board.nextBoards(base);
-
-        /*
-        // add next boards as children to current board
-        for (Board b: nextBoards) {
-            if (MyTreeNode.contains(root, b.getLayout()) == 0){
-                trav.addChild(b.getLayout());
-                List<MyTreeNode> tst = new ArrayList();
-
-                MyTreeNode.getNode(b.getLayout(), tst, trav);
-                MyTreeNode nd = tst.get(0);
-
-                load(nd);
-            }
-        }
-        */
-
-        //add next boards as children to current board
-        for (Board b : nextBoards){
-            MyTreeNode nd = MyTreeNode.getNode(b.getLayout(), root);
-
-            if (nd == null){
-                trav.addChild( b.getLayout() );
-                System.out.println("Board number " + count + " loaded: " + b.getLayout());
-
-                nd = MyTreeNode.getNode(b.getLayout(), root);
-                load(nd);
-
-            }
-
-            else {
-                System.out.println("!!! Board number " + count + " already in memory");
-            }
-
-            count++;
-        }
     }
 }
